088608cc3b05d60e8532f3c7f4d02391e6514cd1
markdaniel1982 <>
2023-06-24 11:29:21 +0000

    create profile model and migrate

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=255, blank=True)
+    profile_image = models.ImageField(
+        upload_to='images/', default='../profileplaceholder_niir6l.png'
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)
