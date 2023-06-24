2aba286a9be2397244dd17ae7b4619b6eaa32251
markdaniel1982 <>
2023-06-24 14:59:11 +0000

    add user_directory_path for uploaded attachements and add filesize validation

--- tasks/models.py
+++ tasks/models.py
@@ -3,11 +3,15 @@ from django.contrib.auth.models import User
 
 
 class Task(models.Model):
-PRIORITY_CHOICES = (
-    (1, 'URGENT'),
-    (2, 'Normal'),
-    (3, 'Low')
-)
+    PRIORITY_CHOICES = [
+        (1, 'URGENT'),
+        (2, 'Normal'),
+        (3, 'Low')
+    ]
+
+    def user_directory_path(instance, filename):
+        # files will be uploaded to MEDIA_ROOT / user_<id>/<filename>
+        return 'user_{0}/{1}'.format(instance.user.id, filename)
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
@@ -16,7 +20,11 @@ PRIORITY_CHOICES = (
     content = models.TextField(blank=True)
     priority = models.IntegerField(choices=PRIORITY_CHOICES)
     due_date = models.DateTimeField()
-    attachments = models.FileField(upload_to='/media/')
+    attachments = models.FileField(
+        upload_to=(upload_to=user_directory_path),
+        default='../default_task_j4ryhk',
+        blank=True,
+        )
 
     class Meta:
         ordering= ['due_date']
--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from tasks.models import tasks
+from tasks.models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
@@ -7,6 +7,13 @@ class TaskSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def validate_attachments(self, value):
+        if value.size > 1024 * 1024 * 5:
+            raise.serializers.ValidationError(
+                'File size is larger than 5mb'
+            )
+        return value
 
     def get_is_owner(self, obj):
         request = self.context['request']
