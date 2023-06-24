7afac1268b7521d62a94e0453083dd49ab6d1154
markdaniel1982 <>
2023-06-24 17:12:16 +0000

    added TaskDetail view to add, edit and delete tasks

--- tasks/views.py
+++ tasks/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Task
 from .serializers import TaskSerializer
+from taskmonkeyapi.permissions import IsOwnerOrReadOnly
 
 
 class TaskList(APIView):
@@ -28,3 +29,42 @@ class TaskList(APIView):
             serializer.save(owner=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TaskDetail(APIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = TaskSerializer
+
+    def get_object(self, pk):
+        try:
+            task = Task.objects.get(pk=pk)
+            self.check_object_permissions(self.request, task)
+            return task
+        except Task.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        task = self.get_object(pk)
+        serializer = TaskSerializer(
+            task, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        task = self.get_object(pk)
+        serializer = TaskSerializer(
+            task, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        task = self.get_object(pk)
+        post.delete()
+        return Response(
+            status=status_204_NO_CONTENT
+        )
