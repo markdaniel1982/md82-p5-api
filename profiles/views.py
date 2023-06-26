56b7e1601493f21c8daa0eb4d76d61f9c02c208a
markdaniel1982 <>
2023-06-26 10:19:12 +0000

    remove old profile view code after refactor

--- profiles/views.py
+++ profiles/views.py
@@ -20,29 +20,3 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-
-# class ProfileDetail(APIView):
-#     serializer_class = ProfileSerializer
-#     permission_classes = [IsOwnerOrReadOnly]
-
-#     def get_object(self, pk):
-#         try:
-#             profile = Profile.objects.get(pk=pk)
-#             self.check_object_permissions(self.request, profile)
-#             return profile
-#         except Profile.DoesNotExist:
-#             raise Http404
-
-#     def get(self, request, pk):
-#         profile = self.get_object(pk)
-#         serializer = ProfileSerializer(profile, context={'request': request})
-#         return Response(serializer.data)
-
-#     def put(self, request, pk):
-#         profile = self.get_object(pk)
-#         serializer = ProfileSerializer(
-#             profile, data=request.data, context={'request': request})
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
