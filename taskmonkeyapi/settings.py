0fb1847cc37ba7f538918463bfc51bcd80c25e11
markdaniel1982 <>
2023-06-26 11:34:50 +0000

    install dj-rest-auth and setup urls for auth use

--- taskmonkeyapi/settings.py
+++ taskmonkeyapi/settings.py
@@ -54,6 +54,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'rest_framework',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
 
     'profiles',
     'tasks',
