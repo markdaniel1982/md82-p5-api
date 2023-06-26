2c68cbc39b0a1d669d44599b685bad3334b668aa
markdaniel1982 <>
2023-06-26 12:55:32 +0000

    remove whitespace in blank lines from 'problems' tab in CA

--- comments/serializers.py
+++ comments/serializers.py
@@ -16,13 +16,12 @@ class CommentSerializer(serializers.ModelSerializer):
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
-    
+
     def get_created_on(self, obj):
         return naturaltime(obj.created_on)
-    
+
     def get_updated_on(self, obj):
         return naturaltime(obj.updated_on)
-
 
     class Meta:
         model = Comment
@@ -37,6 +36,7 @@ class CommentSerializer(serializers.ModelSerializer):
             'updated_on',
             'content',
         ]
+
 
 class CommentDetailSerializer(CommentSerializer):
     task = serializers.ReadOnlyField(source='task.id')
