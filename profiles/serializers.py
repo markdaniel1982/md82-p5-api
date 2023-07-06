b70d1ec3b6981b3977aceb476c56e7ff18f4c82e
markdaniel1982 <>
2023-07-06 17:13:20 +0000

    add comments_count to profile_serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -8,6 +8,7 @@ class ProfileSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     tasks_count = serializers.ReadOnlyField()
+    comments_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
