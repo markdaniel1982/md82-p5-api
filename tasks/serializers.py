a0445c9e01f7c768610f909b9e7c3553c69bbfe4
markdaniel1982 <>
2023-07-06 12:31:49 +0000

    add created_on & updated_on to task serializer

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from .models import Task
 from comments.models import Comment
 
@@ -9,9 +10,18 @@ class TaskSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
+    created_on = serializers.SerializerMethodField()
+    updated_on = serializers.SerializerMethodField()
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_created_on(self, obj):
+        return naturaltime(obj.created_on)
+
+    def get_updated_on(self, obj):
+        return naturaltime(obj.updated_on)
 
     class Meta:
         model = Task
@@ -22,6 +32,7 @@ class TaskSerializer(serializers.ModelSerializer):
             'profile_id',
             'profile_image',
             'created_on',
+            'updated_on',
             'title',
             'content',
             'priority',
