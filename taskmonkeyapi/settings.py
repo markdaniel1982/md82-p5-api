b71f8de5cfc8364d0af502bf958ca098f41dcaec
markdaniel1982 <>
2023-09-29 13:05:40 +0000

    add security.whitenoise to middleware

--- taskmonkeyapi/settings.py
+++ taskmonkeyapi/settings.py
@@ -99,6 +99,7 @@ INSTALLED_APPS = [
 ]
 SITE_ID = 1
 MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
